64b0e8b54833f5de74f4e636410d66358fee7abd
Tristan Mayshark <tristan.mayshark@oracle.com>
2017-08-10 00:55:47 -0400

    sort of works

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ else {
         console.log('Face.x', face.x);
         jimp.read(process.env.INPUT_FILE).then(function (inputFile) {
             jimp.read('./images/catface.png').then(function (cat) {
+                destFile = process.env.INPUT_FILE.split('/')[2].split('.')[0];                cat.resize(face.width*1.1, face.height*1.1);
                 inputFile.composite(cat, face.x, face.y)
-                .write('./images/cattedFace.png');
+                .write(`./output/${destFile}-catted.png`);
             }).catch (function (err) {
                 console.error(err);
             });
